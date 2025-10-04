e904bdfea53c90f349da050f8d20ab50f0028449
alejandro <alejandrozheng666@gmail.com>
2025-10-04 17:22:00 +0200

    update times.c

--- times.c
+++ times.c
@@ -15,6 +15,7 @@
 #include <assert.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /***************************************************/
@@ -97,6 +98,8 @@ short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num
 {
   
   PTIME_AA time = NULL;
+  int i;
+  int n_times = 5;
   
   assert(method != NULL);
   assert(file != NULL);
@@ -104,18 +107,31 @@ short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num
   assert(num_min < num_max);
   assert(num_min <= n_perms);
   assert(num_max >= n_perms);
-
-  /*Malloc PTIME_AA*/
-  time = malloc(1*sizeof(TIME_AA));
-  if (time == NULL)
+  
+  for(i = 0; num_min+ i*incr <= num_max; i++)
   {
-    return ERR;
-  }
-  
-  
 
-  
-  
+    /*Malloc PTIME_AA*/
+    time = malloc(1*sizeof(TIME_AA));
+    if (time == NULL)
+    {
+      return ERR;
+    }
+
+    if(average_sorting_time(method,n_perms,num_min+i*incr, time) == OK)
+    {
+      if (save_time_table(file,time,n_times) == ERR)
+      {
+        free(time);
+        return ERR;
+      }
+    }
+
+    free(time);
+  }
+
+  return OK;
+
 }
 
 /***************************************************/
@@ -125,7 +141,24 @@ short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num
 /***************************************************/
 short save_time_table(char* file, PTIME_AA ptime, int n_times)
 {
-  /* your code */
+  FILE *pf = NULL;
+
+  assert(file != NULL);
+  assert(ptime != NULL);
+  assert(n_times > 0);
+
+  pf = fopen(file,"a");
+  if (pf == NULL)
+  {
+    return ERR;
+  }
+
+  fprintf(pf, "%d %f %f %d %d \n", ptime->N, ptime->time, ptime->average_ob, ptime->max_ob, ptime->min_ob);
+
+  fclose(pf);
+
+  return OK;
+
 }
 
 
